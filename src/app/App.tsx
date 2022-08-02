e4acac1e65fa5ae8327a19be9e3e0e29cce304ec
Damian Jancewicz <62393624+dilejt@users.noreply.github.com>
2022-08-02 17:18:38 +0200

    feat: meta env var

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,9 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProviders } from '@/providers/AppProviders';
 import { AppRoutes } from '@/routing/AppRoutes';
 
-const openReactQueryDevtools = process.env.NODE_ENV === 'development';
-
 export const App = () => (
   <AppProviders>
     <AppRoutes />
-    {openReactQueryDevtools && <ReactQueryDevtools initialIsOpen={false} />}
+    {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
   </AppProviders>
 );
