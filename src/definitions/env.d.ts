0e813571cb0b89b27058e557b897e5f548c58dd6
Damian Jancewicz <62393624+dilejt@users.noreply.github.com>
2022-08-02 21:25:23 +0200

    Revert "Fix/npm update (#5)"
    This reverts commit 590ad3f777145905338bcc6111ed2bbc3e30fae9.

--- src/definitions/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_ENV: 'localhost' | 'develop' | 'test';
-  readonly VITE_API_URL: string;
-  readonly VITE_API_TOKEN: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- vite.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite-plugin-svgr/client" />
-import type { UserConfig as VitestUserConfig } from 'vitest/config';
-
-declare module 'vite' {
-  export interface UserConfig {
-    test?: VitestUserConfig['test'];
-  }
-}
