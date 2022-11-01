02a544f7df73e7115aebb5f98f742c4efb32dfad
Damian Jancewicz <62393624+dilejt@users.noreply.github.com>
2022-11-01 16:40:36 +0100

    fix: redirect after order add

--- src/app/ordersAdd/hooks/useAddOrders/useAddOrdersEffect.ts
+++ src/app/ordersAdd/hooks/useAddOrders/useAddOrdersEffect.ts
@@ -5,9 +5,16 @@ import { OrdersAddFormData } from '../../ordersAddForm/OrdersAddForm.types';
 import { useAddOrders } from './useAddOrders';
 
 import { setFieldsError } from '@/utils/form/setFieldsError/setFieldsError';
+import { useNavigate } from '@/hooks/useNavigate/useNavigate';
+import { AppLinks } from '@/routing/AppRoutes.types';
 
 export const useAddOrdersEffect = (form: UseFormReturn<OrdersAddFormData>) => {
+  const navigate = useNavigate();
+
   return useAddOrders({
+    onSuccess: () => {
+      navigate(AppLinks.Orders);
+    },
     onError: (error) => {
       setFieldsError({
         form,
