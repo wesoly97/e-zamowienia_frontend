0a40a85839123a3b97f04580aef44bd419df8923
Damian Jancewicz <62393624+dilejt@users.noreply.github.com>
2022-11-05 19:30:44 +0100

    fix: remove axios timeout for files uploading

--- src/hooks/useAxios/axiosConfig/axiosConfig.ts
+++ src/hooks/useAxios/axiosConfig/axiosConfig.ts
@@ -3,7 +3,6 @@ export const getAxiosConfig = () => {
     headers: {
       'Content-Type': 'application/json',
     },
-    timeout: 5000,
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
   };
