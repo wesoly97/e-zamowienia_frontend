623a2bfdacee10872c41ff4c263970f975c80d7b
Damian Jancewicz <62393624+dilejt@users.noreply.github.com>
2022-11-26 16:09:21 +0100

    fix: ArrayLike type

--- src/utils/api/formData/formatFormData.ts
+++ src/utils/api/formData/formatFormData.ts
@@ -3,7 +3,7 @@ import { isFileArray } from './formatFormData.typeguards';
 export const formatFormData = <TData>(data: TData) => {
   const formData = new FormData();
 
-  Object.entries(data as ArrayLike<unknown>).map(([key, value]) => {
+  Object.entries(data as ArrayLike<TData>).map(([key, value]) => {
     if (isFileArray(value as File[])) {
       return (value as File[]).map((file) => {
         formData.append(key, file);
